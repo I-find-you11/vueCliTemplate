8bc40c01fcf99ebfee657c21fc505695c53c2aa1
11 <841850130@qq.com>
2020-11-20 22:05:46 +0800

    增加根目录默认跳转

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,18 @@ export default new Router({
   mode:'history',
   routes: [
     {
+      path: '/',
+      redirect: '/index'
+    },
+    {
       path: '/index',
       name: 'index',
       component:  () => import('@/page/index'),
     },
     {
-      path: '/index',
-      name: 'index',
-      component:  () => import('@/page/index')
+      path: '/404',
+      name: 'notFound',
+      component:  () => import('@/page/404')
     },
     {
       path: '*',
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    // proxy: {
+    //   '/api': {
+    //     target: '',  //跨域地址
+    //     changeOrigin: true,  // 是否跨域
+    //     ws: true,  // 是否使用https
+    //     pathRewrite: {
+    //       '^/api': ''
+    //     }
+    //   }
+    // }
+  }
 }
